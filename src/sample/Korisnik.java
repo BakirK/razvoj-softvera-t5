7bbbd9e59e00958b120adb38ca6cb0ce03c475d5
BakirK <bakir.karovic@gmail.com>
2019-04-10 09:23:19 +0200

    Dodan izgled interface-a

---
+++ src/sample/Korisnik.java
@@ -0,0 +1,71 @@
+package sample;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class Korisnik {
+    private SimpleStringProperty ime;
+    private SimpleStringProperty prezime;
+    private SimpleStringProperty email;
+    private SimpleStringProperty username;
+    private  SimpleStringProperty password;
+
+    public String getIme() {
+        return ime.get();
+    }
+
+    public SimpleStringProperty imeProperty() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime.set(ime);
+    }
+
+    public String getPrezime() {
+        return prezime.get();
+    }
+
+    public SimpleStringProperty prezimeProperty() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime.set(prezime);
+    }
+
+    public String getEmail() {
+        return email.get();
+    }
+
+    public SimpleStringProperty emailProperty() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email.set(email);
+    }
+
+    public String getUsername() {
+        return username.get();
+    }
+
+    public SimpleStringProperty usernameProperty() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username.set(username);
+    }
+
+    public String getPassword() {
+        return password.get();
+    }
+
+    public SimpleStringProperty passwordProperty() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password.set(password);
+    }
+}
