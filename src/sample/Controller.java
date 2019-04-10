2e4f1555eb2c6ef0bc0b198c6949da855bc36231
BakirK <bakir.karovic@gmail.com>
2019-04-10 10:01:01 +0200

    Dodan fx id svakom text box-u

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,25 @@
 package sample;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+
 public class Controller {
+
+    public TextField prezimeFld;
+    public TextField imeFld;
+    public TextField passwordFld;
+    public TextField emailFld;
+    public TextField usernameFld;
+
+    @FXML
+    public void initialize() {
+
+    }
+
 }
+
+
+
+
