710864d48c91f30e7ffc0a26dc0ba22002bb4070
BakirK <bakir.karovic@gmail.com>
2019-04-17 09:11:06 +0200

    Dodan folder Tests sa testnom klasom KorisnikTest

---
+++ Tests/sample/KorisnikTest.java
@@ -0,0 +1,72 @@
+package sample;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KorisnikTest {
+
+    @Test
+    void getIme() {
+    }
+
+    @Test
+    void imeProperty() {
+    }
+
+    @Test
+    void setIme() {
+    }
+
+    @Test
+    void getPrezime() {
+    }
+
+    @Test
+    void prezimeProperty() {
+    }
+
+    @Test
+    void setPrezime() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void emailProperty() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void getUsername() {
+    }
+
+    @Test
+    void usernameProperty() {
+    }
+
+    @Test
+    void setUsername() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void passwordProperty() {
+    }
+
+    @Test
+    void setPassword() {
+    }
+/*
+    @Test
+    void toString() {
+    }*/
+}
